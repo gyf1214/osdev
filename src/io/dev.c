e0d4bbfed3f568b1478fe21b154d751a3d67c826
gyf1214 <gyf1214@gmail.com>
2017-01-16 10:37:21 +0800

    add dev control api

--- src/io/dev.c
+++ src/io/dev.c
@@ -8,6 +8,7 @@ void initDev() {
     devCOM1.info = SerialCOM1;
     devCOM1.read = NULL;
     devCOM1.write = serialWrite;
+    devCOM1.control = NULL;
 }
 
 void devWrite(dev_t *dev, const char *data, size_t n) {
@@ -21,5 +22,11 @@ size_t devRead(dev_t *dev, char *buf, size_t n) {
         return dev -> read(dev -> info, buf, n);
     } else {
         return 0;
+    }
+}
+
+void devControl(dev_t *dev, uint32_t sig) {
+    if (dev -> control) {
+        dev -> control(dev -> info, sig);
     }
 }
--- src/io/dev.h
+++ src/io/dev.h
@@ -5,16 +5,19 @@
 
 typedef void (*write_t)(uint32_t, const char *, size_t);
 typedef size_t (*read_t)(uint32_t, char *, size_t);
+typedef void (*control_t)(uint32_t, uint32_t);
 
 typedef struct {
     uint32_t info;
     read_t read;
     write_t write;
+    control_t control;
 }dev_t;
 
 void initDev(void);
 void devWrite(dev_t *dev, const char *data, size_t n);
 size_t devRead(dev_t *dev, char *buf, size_t n);
+void devControl(dev_t *dev, uint32_t sig);
 
 extern dev_t devCOM1;
 

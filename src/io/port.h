b338f70c7c3ec5300d4618e267070db407404d11
gyf1214 <gyf1214@gmail.com>
2017-01-22 14:47:13 +0800

    solve a bug

--- src/io/port.h
+++ src/io/port.h
@@ -12,7 +12,7 @@ uint16_t inw(uint16_t port);
 uint32_t inl(uint16_t port);
 
 void insb(uint16_t port, uint8_t *buf, size_t count);
-void insw(uint16_t port, uint8_t *buf, size_t count);
-void insl(uint16_t port, uint8_t *buf, size_t count);
+void insw(uint16_t port, uint16_t *buf, size_t count);
+void insl(uint16_t port, uint32_t *buf, size_t count);
 
 #endif

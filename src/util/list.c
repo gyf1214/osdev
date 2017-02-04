0099b81c386f4449b87ef1efe307e2c469ab2f8f
gyf1214 <gyf1214@gmail.com>
2017-02-04 11:33:07 +0800

    solve a bug in list

--- src/util/list.c
+++ src/util/list.c
@@ -1,5 +1,9 @@
 #include "util/list.h"
 
+void listInit(list_t *now) {
+    now -> next = now -> prev = now;
+}
+
 void listAppend(list_t *now, list_t *prev) {
     list_t *next = prev -> next;
     prev -> next = now;
@@ -11,4 +15,8 @@ void listAppend(list_t *now, list_t *prev) {
 void listDelete(list_t *now) {
     now -> next -> prev = now -> prev;
     now -> prev -> next = now -> next;
+}
+
+int listEmpty(list_t *now) {
+    return now -> next == now;
 }
--- src/util/list.h
+++ src/util/list.h
@@ -4,14 +4,15 @@
 #include "util/type.h"
 
 #define listEntry(ptr, type, member)    MemberContainer(ptr, type, member)
-#define listInit(name)                  (name) = { .next = &(name), .prev = &(name) }
 
 typedef struct list {
     struct list *next;
     struct list *prev;
 }list_t;
 
+void listInit(list_t *now);
 void listAppend(list_t *now, list_t *prev);
 void listDelete(list_t *now);
+int listEmpty(list_t *now);
 
 #endif

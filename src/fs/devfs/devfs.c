a7ab8b2a55a6a4b1864c2944aa98f36a065b8ed8
gyf1214 <gyf1214@gmail.com>
2017-02-05 23:00:34 +0800

    almost finish devfs

--- src/fs/devfs/devfs.c
+++ src/fs/devfs/devfs.c
@@ -1,5 +1,6 @@
 #include "fs/devfs/devfs.h"
 #include "fs/ramfs/ramfs.h"
+#include "fs/dentry.h"
 #include "fs/fs.h"
 
 vnode_t *devRoot = NULL;
@@ -9,7 +10,32 @@ void initDevfs() {
     devfs -> readSuperblock = ramfsReadSuperblock;
     devfs -> releaseNode = ramfsReleaseNode;
     devfs -> readNode = ramfsReadNode;
+    devfs -> readFile = devfsReadFile;
+    devfs -> writeFile = devfsWriteFile;
     fsRegister(devfs);
 
-    devRoot = vnodeOpen(fsMountAt(devfs, NULL, fsRoot, DevfsMountPoint));
+    devRoot = fsMountAt(devfs, NULL, fsRoot, DevfsMountPoint);
+    vnodeInit(devRoot);
+}
+
+void devfsMountDevice(vnode_t *parent, const char *name, device_t *device) {
+    vnode_t *vnode = ramfsAllocNode(devRoot -> sb);
+    vnode -> info = (uint32_t)device;
+
+    dentry_t *dentry = dentryAlloc(vnode, name);
+    vnodeAppendDentry(parent, dentry);
+}
+
+ssize_t devfsReadFile(vnode_t *vnode, void *buf,
+                      size_t size, offset_t *offset) {
+    Unused(offset);
+    device_t *dev = (device_t *)vnode -> info;
+    return deviceRead(dev, buf, size);
+}
+
+ssize_t devfsWriteFile(vnode_t *vnode, const void *buf,
+                       size_t size, offset_t *offset) {
+    Unused(offset);
+    device_t *dev = (device_t *)vnode -> info;
+    return deviceWrite(dev, buf, size);
 }
--- src/fs/devfs/devfs.h
+++ src/fs/devfs/devfs.h
@@ -1,9 +1,19 @@
 #ifndef __DEVFS
 #define __DEVFS
 
+#include "fs/vnode.h"
+#include "io/device.h"
+
 #define DevfsName       "devfs"
 #define DevfsMountPoint "dev"
 
+extern vnode_t *devRoot;
+
 void initDevfs(void);
+void devfsMountDevice(vnode_t *parent, const char *name, device_t *dev);
+ssize_t devfsReadFile(vnode_t *vnode, void *buf,
+                      size_t size, offset_t *offset);
+ssize_t devfsWriteFile(vnode_t *vnode, const void *buf,
+                       size_t size, offset_t *offset);
 
 #endif
